40bd76e8ca6ac1883376557d555c5e3e54364997
meuta <efanya@gmail.com>
2025-06-16 22:43:54 +0100

    gradle dependencies update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -43,7 +44,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
@@ -54,8 +55,8 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
 
     // ExoPlayer
-    implementation("androidx.media3:media3-exoplayer:1.6.1")
-    implementation("androidx.media3:media3-ui:1.6.1")
+    implementation("androidx.media3:media3-exoplayer:1.7.1")
+    implementation("androidx.media3:media3-ui:1.7.1")
 
     // Gson
     implementation("com.google.code.gson:gson:2.10.1")
