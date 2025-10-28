430810bdbb03f64af5f383da97b1d4a1051095bb
lunatico <efanya@gmail.com>
2025-10-28 02:13:01 +0000

    chore(config): update targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.obrigada_eu.poika"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 2
         versionName = "2.0"
 
