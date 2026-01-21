1f340555d1e819fda8e097a3a2f1d00f1303827b
lunatico <efanya@gmail.com>
2026-01-21 16:54:59 +0000

    fix: add @SerializedName annotations to data models for release obfuscation

--- app/src/main/java/com/obrigada_eu/poika/player/domain/model/SongMetaData.kt
+++ app/src/main/java/com/obrigada_eu/poika/player/domain/model/SongMetaData.kt
@@ -1,14 +1,16 @@
 package com.obrigada_eu.poika.player.domain.model
 
+import com.google.gson.annotations.SerializedName
+
 data class SongMetaData(
-    val title: String,
-    val artist: String,
-    val voiceInstrument: String?,
-    val tracks: List<TrackInfo>,
+    @SerializedName("title") val title: String,
+    @SerializedName("artist") val artist: String,
+    @SerializedName("voiceInstrument") val voiceInstrument: String?,
+    @SerializedName("tracks") val tracks: List<TrackInfo>,
     val folderName: String
 )
 
 data class TrackInfo(
-    val file: String,
-    val name: String
+    @SerializedName("file") val file: String,
+    @SerializedName("name") val name: String
 )
